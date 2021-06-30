4bf2d5bafc1333f174b6b5843ab99225bf72d934
Hafiz Sukmono <74044681+Hafiz-sukmono@users.noreply.github.com>
2021-06-30 13:44:31 +0700

    C++ penjumlahan

---
+++ pertambahan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+
+	int bil1,bil2,jumlah;
+	
+	cout <<"masukkan bilangan 1 : " ; 
+	cin >> bil1;
+	cout <<"masukkan bilangan 2 : " ;
+	cin >> bil2;
+	
+	jumlah = bil1 + bil2;
+	
+	cout << "jumlah : " << jumlah << endl;
+	
+	return 0;
+}
